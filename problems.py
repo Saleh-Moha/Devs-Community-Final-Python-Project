851687acd8fa110b8273c0353e82b3c2417e1123
sale <sm3272692@gmail.com>
2024-10-03 17:59:15 +0300

    Devs

--- problems.py
+++ problems.py
@@ -1,46 +1,51 @@
 import json
+import random
+import string
+from datetime import datetime
+
 
 # staff class
 # =========================================================================================================
 class Staff:
-    # __intit__ => like a constructor 
+    # __intit__ => like a constructor
     def __init__(self) -> None:
         self.staff = self.load_staff()
-               
-    def load_staff(self): # method load is a method to start the file if it does not exist or read its data of it exists
+
+    def load_staff(
+        self,
+    ):  # method load is a method to start the file if it does not exist or read its data of it exists
         try:
-            with open("staff.json","r") as f:
+            with open("staff.json", "r") as f:
                 return json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):  # Handle exceptions
             return []
-        
-    # method save is a method that used to close the file after adding or updating or deleteing somthing from it 
+
+    # method save is a method that used to close the file after adding or updating or deleteing somthing from it
     def save_staff(self):
-        with open("staff.json","w") as f:
-            json.dump(self.staff, f ,indent=4)
+        with open("staff.json", "w") as f:
+            json.dump(self.staff, f, indent=4)
+
     # add a new staff methode
     def add_staff(self):
-            who_is_adding = int(input("enter your id: "))
-            for member in self.staff:
-                if member["id"] == who_is_adding and member["job"] == "manager":
-                    new_member = str(input("enter the name : "))
-                    job = str(input("enter the job : "))
-                    id = int(input("enter the id : "))
-                    self.staff.append({
-                        "name": new_member,
-                        "job": job,
-                        "id": id
-                    })
-                    
-                    self.save_staff()
-                    
-    # delete a staff if it exists 
+        len = 8
+        char = string.ascii_letters + string.digits
+        who_is_adding = int(input("enter your id: "))
+        for member in self.staff:
+            if member["id"] == who_is_adding and member["job"] == "manager":
+                new_member = str(input("enter the name : "))
+                job = str(input("enter the job : "))
+                id = "".join(random.choice(char) for _ in range(len))
+                self.staff.append({"name": new_member, "job": job, "id": id})
+
+                self.save_staff()
+
+    # delete a staff if it exists
     def delete_staff(self):
-        with open("staff.json","r") as f:
+        with open("staff.json", "r") as f:
             who_is_deleting = int(input("enter your id: "))
             for member in self.staff:
                 if member["id"] == who_is_deleting and member["job"] == "manager":
-                    name  = str(input("enter the staff name: "))
+                    name = str(input("enter the staff name: "))
                     for member in self.staff:
                         if member["name"] == name:
                             self.staff.remove(member)
@@ -49,14 +54,16 @@ class Staff:
                 else:
                     print("you are not allowed to do such a thing")
                     break
-    
-    # update staff 
+
+    # update staff
     def update_staff(self):
         who_is_updating = int(input("Enter your ID: "))
         for member in self.staff:
             if member["id"] == who_is_updating:
                 if member["job"] == "manager":
-                    updated_id = int(input("Enter the ID of the person you want to update: "))
+                    updated_id = int(
+                        input("Enter the ID of the person you want to update: ")
+                    )
                     for person in self.staff:
                         if person["id"] == updated_id:
                             answer = input("What do you want to update (name, job)? : ")
@@ -69,55 +76,123 @@ class Staff:
                             else:
                                 print("Invalid update option.")
                             self.save_staff()
-                            print(f"Staff member with ID {updated_id} has been updated.")
+                            print(
+                                f"Staff member with ID {updated_id} has been updated."
+                            )
                             return
                     print(f"Staff member with ID '{updated_id}' not found.")
                 else:
-                    print("You are not allowed to update staff. Only managers can perform this action.")
+                    print(
+                        "You are not allowed to update staff. Only managers can perform this action."
+                    )
                 return
         print("ID not found.")
-        
-        
+
     # get all the staff
     def get_staff(self):
         who_is_getting = int(input("enter your id: "))
         for member in self.staff:
-            if member["id"] == who_is_getting :
-                if member["job"]=="manager": 
+            if member["id"] == who_is_getting:
+                if member["job"] == "manager":
                     print(f"{self.staff}")
                     return
         print("you are not allowed to do this only managers")
-     
-    # search for staff   
-    def search(self):
+
+    # search for staff
+    def search_for_staff(self):
         who_is_searching = int(input("enter your id: "))
         for member in self.staff:
             if member["id"] == who_is_searching and member["job"] == "manager":
-                wanted_staff = input("enter the id,name of the user you wnat to search about: ")
-                found = False 
+                found_mamanger = True
+                wanted_staff = input(
+                    "enter the id,name of the user you wnat to search about: "
+                )
+                found = False
                 for person in self.staff:
                     if wanted_staff.isdigit():
                         if person["id"] == int(wanted_staff):
                             print(person)
                             found = True
                             break
-                # Otherwise, compare as name
-                    elif person["name"].lower() == wanted_staff.lower():  # Case-insensitive name comparison
+                    # Otherwise, compare as name
+                    elif (
+                        person["name"].lower() == wanted_staff.lower()
+                    ):  # Case-insensitive name comparison
                         print(person)
                         found = True
-                        
-                if not found:    
-                    print("Nothing found")
 
-print("what do you want to do? ")
-message = str(input("enter a choise(add,delete,update,search,get,search): ")) 
-if message == "add":             
-    Staff().add_staff()
-elif message == "delete":
-    Staff().delete_staff()
-elif message == "update":
-    Staff().update_staff()
-elif message == "get":
-    Staff().get_staff()
-elif message == "search":
-    Staff().search()
+                if not found:
+                    print("Nothing found")
+        if not found_mamanger:
+            print("you are not allowed to do this only managers")
+# ====================================end of staff class====================================================
+
+
+class Books(Staff):
+    def __init__(self):
+        super().__init__() 
+        self.books = self.load_books()    
+
+    def load_books(self):
+        try:
+            with open("Books.json","r") as f:
+                return json.load(f)        
+        except(FileNotFoundError,json.JSONDecodeError):
+            return[]
+        
+    def save_books(self):
+        with open("Books.json","w") as f:
+            json.dump(self.books,f,indent=4)
+    
+    def add_books(self):
+        who_is_adding = int(input("enter your id: "))
+        char = string.ascii_letters + string.digits
+        len = 8
+        for staff in self.staff:
+            if staff["id"] == who_is_adding:
+                book_name = input("enter the book name: ")
+                book_author = input("enter the book author: ")
+                book_price = float(input("enter the book price: "))
+                who_added_it = staff
+                when_added_it = datetime.now().isoformat()
+                book_id = ''.join(random.choice(char) for _ in range(len))
+                self.books.append(
+                    {
+                        "book_name" : book_name,
+                        "book_author" : book_author,
+                        "book_price" : book_price,
+                        "book_id" : book_id,
+                        "who_added_it" : who_added_it,
+                        "when_added_it" : when_added_it
+                    }
+                )
+                self.save_books()
+
+
+
+
+
+
+
+
+print("Books management , staff management")
+answer = str(input("enter a choise: "))
+if answer == "books":
+    print("what do you want to do? ")
+    message = str(input("enter a choise(add,delete,update,search,get,search): "))
+    if message == "add":
+        Books().add_books()
+
+else:
+    print("what do you want to do? ")
+    message = str(input("enter a choise(add,delete,update,search,get,search): "))
+    if message == "add":
+        Staff().add_staff()
+    elif message == "delete":
+        Staff().delete_staff()
+    elif message == "update":
+        Staff().update_staff()
+    elif message == "get":
+        Staff().get_staff()
+    elif message == "search":
+        Staff().search_for_staff()
